e57fdf1d047c68e52c1fdba9ed4df24ebf4d3fff
Jona Martinez <jonaezeq36@gmail.com>
2022-12-20 05:29:10 -0300

    Verification for nick, email and size

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 const nickInput=document.getElementById("nick");
 const sizeInput=document.getElementById("size");
 const formEntrada=document.getElementById("formEntrada");
-
+const error=document.getElementById("error");
+const email=document.getElementById("email")
 //Function of event
 function comprobarform(){
     //Comprobate changes
@@ -12,11 +13,19 @@ function comprobarform(){
     console.log("Nick Unavailable");
     nickInput.focus();
     event.preventDefault();
+    error.innerText="Write you're nick"
     return false;
+    }else if(email.value==""){
+        console.log("E-mail unavailable");
+        email.focus();
+        event.preventDefault();
+        error.innerText="Write you're email";
+        return false;
     }else if(sizeInput.value=="0"){
         console.log("Select a size plz");
         sizeInput.focus();
-        Event.preventDefault();
+        event.preventDefault();
+        error.innerText="Select a size for the game"
         return false;
     }
     return true;
